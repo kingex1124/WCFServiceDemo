ad11cde17fb6253fc7576baa9c3d734d28e5afbf
kevan.chen <kevan.chen@sinopa.com>
2020-06-22 17:15:24 +0800

    加入掛載console 用consoleClient呼叫WCF

--- ConsoleAppWCFService/Program.cs
+++ ConsoleAppWCFService/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using WcfServiceLibraryDemo;
 
 namespace ConsoleAppWCFService
 {
@@ -10,6 +12,10 @@ namespace ConsoleAppWCFService
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Service is running...");
+            ServiceHost myHost = new ServiceHost(typeof(Service1));
+            myHost.Open();
+            Console.ReadLine();
         }
     }
 }
---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleClient.WCFServiceLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IService1 proxy =  new Service1Client();
+            string result = proxy.GetData(5);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
